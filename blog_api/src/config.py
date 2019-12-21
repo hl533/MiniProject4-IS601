90433a5fe57bd309274fd17e1367b5c30cc4c3e7
harry <hl533@njit.edu>
2019-12-21 03:33:16 -0500

    FEATURE: Added tests folder, edited config file, added new init and test_user.py

--- blog_api/src/config.py
+++ blog_api/src/config.py
@@ -20,7 +20,7 @@ class Production(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
- class Testing(object):
+class Testing(object):
     """
     Development environment configuration
     """
